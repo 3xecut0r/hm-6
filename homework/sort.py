5f6b302cccaab3f80a507843d96c1db32e774330
Oleksii Panpukha <58779018+3xecut0r@users.noreply.github.com>
2023-02-25 08:47:48 +0100

    Update sort.py

--- homework/sort.py
+++ homework/sort.py
@@ -5,7 +5,6 @@ import os
 import shutil
 
 path = sys.argv[1]
-# p = Path(root)
 extensions = {
 "images":['.jpeg', '.png', '.jpg', '.svg'],
 "video":['.avi', '.mp4', '.mov', '.mkv'],
@@ -28,26 +27,26 @@ except FileExistsError:
     pass
 
 
-        
-
 def sorter(path):
     all = os.listdir(path)
+    
     for x in all:
         name, ext = os.path.splitext(x)
-        # ext = x.split(".")[-1]
-        # name = x.split("\\")[-1]
+        
         if x in all:
             if not os.path.isdir(x):
                 for key, val in extensions.items():
                     if ext in val:
-                        os.rename(f"{path}\\{x}", f"{path}\\{key}\\{x}")
+                        os.rename(f"{path}/{x}", f"{path}/{key}/{x}")
                     else:
                         if x == "sort.py":
                             pass
+                        
             elif x in extensions.keys():
                 pass
+            
             elif os.path.isdir(x):
-                os.rename(f"{path}\\{x}", f"{path}\\others\\{x}")
+                os.rename(f"{path}/{x}", f"{path}/others/{x}")
                 
 # def unpack_arch(path):
 #     way = [i for i in os.listdir(f"{path}\\archives\\")]
@@ -60,9 +59,11 @@ def sorter(path):
 
 
 
-# sorter(path)   
+sorter(path)   
 
 def normalize(path):
+    text = ''
+    
     table = {
         'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'ґ': 'g',
         'д': 'd', 'е': 'e', 'є': 'ie', 'ж': 'zh', 'з': 'z',
@@ -77,10 +78,11 @@ def normalize(path):
         'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S',
         'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'KH', 'Ц': 'TS',
         'Ч': 'CH', 'Ш': 'SH', 'Щ': 'SHCH', 'Ь': '', 'Ю': 'IU',
-        'Я': 'IA', ' ': ' ', '/':'/', '~':'~', 'w':'w'
+        'Я': 'IA', ' ': ' ', '/':'/', '~':'~', 'w':'w', 'c':'c',
+        '.':'.', 'x':'x'
     }
-    text = ''
-    # directory, filename = os.path.split(path)
+    
+    
     for i in path:
         if i not in table.values():
             if i in table.keys():
@@ -91,83 +93,23 @@ def normalize(path):
                 text+='_'
         else:
             text+=i
+            
     return os.rename(f'{path}', f'{text}')
-    # trans = ''.join(table.get(i, i) for i in filename)
-    # return os.path.join(directory, trans)
-    # for i in path:
-    #     if i in table:
-    #         text += table[i]
-    #         print(text)
-    #     else:
-    #         text += i +'/'
-    # print(text)
-normalize(path)
     
 
-
-
-
-
-# def normalize(name):
-#     full = ""
-#     data = {
-#         'а': 'a',
-#         'б': 'b',
-#         'в': 'v',
-#         'г': 'h',
-#         'ґ': 'g',
-#         'д': 'd',
-#         'е': 'e',
-#         'є': 'ie',
-#         'ж': 'zh',
-#         'з': 'z',
-#         'и': 'y',
-#         'і': 'i',
-#         'ї': 'i',
-#         'й': 'i',
-#         'к': 'k',
-#         'л': 'l',
-#         'м': 'm',
-#         'н': 'n',
-#         'о': 'o',
-#         'п': 'p',
-#         'р': 'r',
-#         'с': 's',
-#         'т': 't',
-#         'у': 'u',
-#         'ф': 'f',
-#         'х': 'kh',
-#         'ц': 'ts',
-#         'ч': 'ch',
-#         'ш': 'sh',
-#         'щ': 'shch',
-#         'ь': '',
-#         'ў': 'u',
-#         'ы': 'y',
-#         'э': 'e',
-#         'ю': 'iu',
-#         'я': 'ia'
-#     }
-#     word = compile(r'[^a-zA-Z0-9]')
-#     name = ''.join([data.get(i, i) for i in name])
-#     name = word.sub('_', name)
-#     return name
-# a = input()       
-# result = normalize(a)
-# print(result)
-
-
-
-# def read_files_and_folders(path):
-#     for item in os.listdir(path):
-#         item_path = os.path.join(path, item)
-#         if os.path.isfile(item_path):
-#             print(item_path)
-#         elif os.path.isdir(item_path):
-#             print(item_path)
-#             read_files_and_folders(item_path)
-# res = read_files_and_folders(path)
-# print(res)
+def read_files_and_folders(path):
+    
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+        
+        if os.path.isfile(item_path):
+            normalize(item_path)
+            
+        elif os.path.isdir(item_path):
+            normalize(item_path)
+            read_files_and_folders(item_path)
+            
+read_files_and_folders(path)
 
 
 
