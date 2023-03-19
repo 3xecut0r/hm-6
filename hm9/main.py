97104968b27373f5c146e70c7df5b3cede3c1fcc
Antibiotic <ofsposs@gmail.com>
2023-03-19 13:06:25 +0100

    Create main.py

---
+++ hm9/main.py
@@ -0,0 +1,75 @@
+import re
+
+contacts = {}
+
+def hello():
+    print("How can I help you?")
+
+def show_all():
+    for key, value in contacts.items():
+        print(f"{key}: {value}")
+    l = len(key)
+    print(f"List contain <{l}> contacts")
+        
+def change(name, phone):
+    contacts.update({name:phone})
+        
+def add(name, phone):
+    contacts.update({name:phone})
+    print(f"I add contact for <{name}> with phone <{phone}>")
+
+def phone(name):
+    for k,v in contacts.items():
+        if name == k:
+            print(v)
+
+def input_error(func):
+    def wrapper():
+        try:
+            return func()
+        except KeyError:
+            print("Give me a name and phone please")
+        except ValueError:
+            print("Number is incorrect")
+        except IndexError:
+            print("There is no contact with that name")
+    return wrapper()
+            
+        
+@input_error
+def main():
+    while True:
+        command = input(">>> ").strip().lower()
+
+        if "show all" in command:
+            return show_all()
+        
+        elif "hello" in command:
+            return hello()
+        
+        elif "add" in command:
+            res = command[4:].strip().split()
+            return add(res[0], res[1])
+            
+        elif command == ".":
+            break
+        
+        elif command in ["good bye", "close", "exit"]:
+            print("Good bye!")
+            break
+        
+        elif "change" in command:
+            res = command[7:].strip().split()
+            return change(res[0], res[1])
+            
+        elif "phone" in command:
+            name = command[6:]
+            return phone(name)
+        
+        else:
+            return command
+        
+            
+
+if __name__ == "__main__":
+    main()
